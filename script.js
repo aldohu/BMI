7b5a3cf97cde2ae8140bd47b9f97860f0bbcaedd
Aldin <alhuric89@gmail.com>
2022-03-25 01:57:44 +0100

    Add prompt for John's and Mark's info

---
+++ script.js
@@ -0,0 +1,91 @@
+/*
+#Variables and datatypes
+
+*/ 
+/*
+
+var firstname = "John";
+
+console.log(firstname);
+
+var lastName = "Smith";
+var age = 20;
+
+var fullAge = true;
+
+console.log(fullAge);
+
+console.log(lastName, age)
+
+
+/* 
+#Variable mutation and type coercion
+*/
+
+/*
+var firstName = "John";
+
+var age = 20;
+
+console.log(`${firstName} is ${age}`);
+
+var job, isMarried;
+
+
+job = "Teacher";
+isMarried = false;
+
+console.log(`${firstName} is a ${age} year old. Is he married ${isMarried}`)
+
+
+age = "twenty";
+job = "driver";
+
+
+var name = prompt("What is his last name?")
+console.log(name);*/
+
+/*
+#Basic operators
+*/
+/*
+//Math operators
+var year,yearJohn,yearMark;
+year = 2022;
+yearJohn = year - 32;
+
+console.log(yearJohn);
+
+yearMark = year -37;
+
+//Logical operators
+var logic = 3 < 4;
+
+//typeoff operator
+
+console.log(typeof logic);
+*/
+/*
+#Operator precedence
+*/
+/*
+var now = 2022;
+var yearJohn = 1989;
+var fullAge = 18;
+var isFullAge = now - yearJohn >= fullAge;
+var ageJohn = now - yearJohn;
+var ageMark = 37;
+var avg = (ageJohn + ageMark) / 2;
+console.log(avg);
+console.log(isFullAge);
+*/
+
+
+var BMI, markMass, markHeight, johnMass, johnHeight;
+
+ markMass = Number(prompt("What is Mark's mass"));
+ markHeight = Number(prompt("What is Mark's height"));
+
+ johnMass = Number(prompt("What is Mark's mass"));
+johnHeight = Number(prompt("What is Mark's height"));
+
